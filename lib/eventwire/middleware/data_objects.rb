e3a554aa00a38bbe4f31d58bd0c2e75a88652066
Morgan Hallgren <morgan.hallgren@gmail.com>
2012-01-02 15:34:59 +0100

    Added puts

--- lib/eventwire/middleware/data_objects.rb
+++ lib/eventwire/middleware/data_objects.rb
@@ -5,6 +5,7 @@ module Eventwire
     class DataObjects < Base
       def subscribe(event_name, handler_id, &handler)
         @app.subscribe event_name, handler_id do |data|
+          puts "biuld hash from #{data}"
           handler.call build_event(data)
         end
       end
