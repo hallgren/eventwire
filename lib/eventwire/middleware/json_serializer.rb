3e0cd7a2e58698390b61ec5ff2f596198a9fa169
Morgan Hallgren <morgan.hallgren@gmail.com>
2012-01-03 10:26:47 +0100

    removed handle event from json middleware

--- lib/eventwire/middleware/json_serializer.rb
+++ lib/eventwire/middleware/json_serializer.rb
@@ -14,10 +14,10 @@ module Eventwire
         @app.publish event_name, event_data.to_json
       end
 
-      def handle_event(event_name, event_data)
-        puts "handle_event: JSONSerializer #{event_name} #{event_data}"
-        @app.handle_event event_name, event_data
-      end
+      #def handle_event(event_name, event_data)
+        #puts "handle_event: JSONSerializer #{event_name} #{event_data}"
+        #@app.handle_event event_name, event_data
+      #end
       
       private
       
